429eb1e2a589c2a484f2aaaf968766e876931123
lolzini <97997498+lolzini@users.noreply.github.com>
2025-03-20 22:43:46 -0600

    update: astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,12 @@ import netlify from "@astrojs/netlify";
 export default defineConfig({
   integrations: [tailwind()],
   adapter: netlify(),
+  i18n: {
+    defaultLocale: "es",
+    locales: ["en", "es"],
+    routing: {
+      prefixDefaultLocale: true,
+      redirectToDefaultLocale: true,
+    },
+  },
 });
